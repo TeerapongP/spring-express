a65939ae7f34c3f46937da9adbfd9bc7c3ef3149
TeerapongP <thirapongp7@gmail.com>
2024-12-15 21:29:12 +0700

    first commit

---
+++ src/main/java/spring_express/spring_express/SpringExpressApplication.java
@@ -0,0 +1,13 @@
+package spring_express.spring_express;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_express/spring_express/SpringExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_express.spring_express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
